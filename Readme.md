25e84d3ab2f4b6860aa657f6a3e6d4ccbe2904fa
Hsiaoming Yang <me@lepture.com>
2014-02-09 17:04:42 +0800

    Init commits

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# dom-parser
+
+  Browser compatible document parser. Inspired by [rails/turbolinks](https://github.com/rails/turbolinks).
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install yuehu/dom-parser
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * dom-parser
+ *
+ * Browser compatible DOM parser.
+ */
+
+// Thanks to rails/turbolinks
+
+function useParser(html) {
+  var d = new DOMParser();
+  return d.parseFromString(html, 'text/html');
+}
+
+function useDom(html) {
+  var d = document.implementation.createHTMLDocument('');
+  d.documentElement.innerHTML = html;
+  return d;
+}
+
+function useWrite(html) {
+  var d = document.implementation.createHTMLDocument('');
+  d.open('replace');
+  d.write(html);
+  d.close();
+  return d;
+}
+
+var snippet = '<html><body><p>test';
+
+function getParser() {
+  var ref ;
+  try {
+    if (window.DOMParser) {
+      ref = useParser(snippet);
+      return useParser;
+    }
+  } catch (err) {
+    ref = useDom(snippet);
+    return useDom;
+  } finally {
+    if (ref && ref.body && ref.body.childNodes.length === 1) return;
+    return useWrite;
+  }
+}
+
+var parser = getParser();
+
+function dom(html) {
+  if (parser) return parser(html);
+  return null;
+}
+dom.parser = parser;
+
+module.exports = dom;
